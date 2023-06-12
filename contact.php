2b1bf5a8798684969494090776179c76a2ef3385
Dias077 <71216883+Dias077@users.noreply.github.com>
2023-06-13 03:00:31 +0600

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,20 @@
+<?php
+// require("db.php");
+// if((isset($_POST['name'])&& $_POST['name'] !='') && (isset($_POST['phone'])&& $_POST['phone'] !='') && (isset($_POST['subject'])&& $_POST['subject'] !=''))
+// {
+//     $yourName = $conn->real_escape_string($_POST['name']);
+//     $yourPhone = $conn->real_escape_string($_POST['phone']);
+//     $yourSubject = $conn->real_escape_string($_POST['subject']);
+//     $sql="INSERT INTO core (name, phone,subject) VALUES ('".$yourName."','".$yourPhone."','".$yourSubject."')";
+//     if(!$result = $conn->query($sql)){
+//         die('There was an error running the query [' . $conn->error . ']');
+//     }
+//     else{
+//         echo 'Ваши данные были сохранены успешно!';
+//     }
+// }
+// else
+// {
+//     echo "Пожалуйста заполните имя, тариф и номер";
+// }
+?>
